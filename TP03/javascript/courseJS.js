f37c3d9e81c24f6f2ad23122e425155f19b37a8b
Maxime <2256947@bdeb.qc.ca>
2023-11-27 10:10:43 -0500

    essai cube mouvement

--- TP03/javascript/courseJS.js
+++ TP03/javascript/courseJS.js
@@ -3,15 +3,49 @@ let contexte;
 let matrice;
 let compteurCouleur = 0;
 
+const dimsprite1 = 48;
+const dimsprite2 = 48;
+const deplacement = 10;
+    let posx1;
+    let posy1;
+    let posx2;
+    let posy2;
+    let touche = {
+         "ArrowUp": false,
+         "ArrowDown": false,
+         "ArrowLeft": false,
+         "ArrowRight": false,
+         "W": false,
+         "A": false,
+         "S": false,
+         "D": false
+        }
+
 const dimensiontuile = 32;
 const couleur = ["darkgreen", "forestgreen"];
+
+
+
+
+
+
+
 window.onload = function(){
+    window.addEventListener("keydown", toucheAppuyee)
+    window.addEventListener("keyup", toucheRelachee)
 
     canevas = document.getElementById('canevas');
     contexte = canevas.getContext('2d');
 
     construireMatrice();
     affichermatrice();
+
+//let ImageSprite1 = new Image(images/CuboneSprites/Cubone_Front)
+    contexte.fillStyle = "blue";
+         posx1 = canevas.width/2 - dimsprite1/2;
+         posy1 = canevas.height/2 - dimsprite1/2;
+
+        window.requestAnimationFrame(boucleJeu);  // le navigateur appellera boucleJeu() au bon moment
 }
     
 function construireMatrice(){
@@ -55,3 +89,82 @@ function nombreEntre(){
     return couleur[compteurCouleur];
     }
  
+    
+
+
+   
+
+   
+
+    function boucleJeu(timeStamp){
+        calculerPosition();
+        
+       affichage();
+
+        window.requestAnimationFrame(boucleJeu);  // le navigateur appellera boucleJeu() au bon moment
+        
+        
+    }
+
+    function affichage(){
+
+     contexte.clearRect(0, 0 ,1000, 500);
+     
+     contexte.fillRect(posx1, posy1, dimsprite1, dimsprite1) 
+
+
+
+    }
+
+    function toucheAppuyee(evenement){
+        if((evenement.key === "ArrowUp") || (evenement.key === "ArrowDown") || (evenement.key === "ArrowLeft") || (evenement.key === "ArrowRight")){
+touche[evenement.key] = true;
+        }
+    }
+
+    function toucheRelachee(evenement){
+        if((evenement.key === "ArrowUp") || (evenement.key === "ArrowDown") || (evenement.key === "ArrowLeft") || (evenement.key === "ArrowRight")){
+touche[evenement.key] = false;
+        }
+    }
+
+    function calculerPosition(){
+   
+       if(touche["ArrowUp"]){
+        posy1 -= deplacement;
+       }
+       if(touche["ArrowDown"]){
+        posy1 += deplacement;
+       }
+       if(touche["ArrowRight"]){
+        posx1 += deplacement;
+       }
+       if(touche["ArrowLeft"]){
+        posx1 -= deplacement;
+       }
+
+       if(posx1 < 0)
+       {
+        posx1 = 0
+
+       }
+
+       if(posx1 > canevas.width - dimsprite1)
+       {
+        posx1 = canevas.width - dimsprite1
+       }
+
+       if(posy1 > (canevas.height - dimsprite1) )
+       {
+        posy1 = canevas.height - dimsprite1
+       }
+
+       if(posy1 < 0 )
+       {
+        posy1 = 0
+       }
+
+    }
+    
+
+
