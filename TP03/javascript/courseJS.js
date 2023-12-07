53c6141ccca2372af3e5cd34ee2dbb687d01b84a
Maxime <2256947@bdeb.qc.ca>
2023-12-07 15:27:38 -0500

    finish

--- TP03/javascript/courseJS.js
+++ TP03/javascript/courseJS.js
@@ -4,7 +4,14 @@ let contexte;
 let matrice;
 let compteurCouleur = 0;
 
+let pointageJoueur1 = 0;
+let pointageJoueur2 = 0;
+
+let activation = true;
+
 let tableauBerry = [];
+
+
 // Constantes pour les dimensions des sprites et les distances de déplacement
 const dimsprite1 = 48;
 const dimsprite2 = 48;
@@ -57,12 +64,12 @@ for (const direction in joueur2ImagesUrl) {
          "a": false,
          "s": false,
          "d": false,
-         "Enter": false,
-         " ": false
+         " ": false,
+         "1": false
         }
 
 // Constantes pour les dimensions des tuiles et les couleurs
-const dimensiontuile = 32;
+const dimensiontuile = 40;
 const couleur = ["pink", "mistyrose"];
 
     // Chargement de la page
@@ -70,11 +77,19 @@ const couleur = ["pink", "mistyrose"];
         // Ajout des écouteurs d'événements pour les touches
         Berry = document.getElementById("BerryImg");
 
+        AffichageJoueur1 = document.getElementById("Score1");
+        AffichageJoueur2 = document.getElementById("Score2");
+
+        AffichageJoueur1.innerHTML = pointageJoueur1;
+        AffichageJoueur2.innerHTML = pointageJoueur2;
+
         window.addEventListener("keydown", toucheAppuyee)
         window.addEventListener("keyup", toucheRelachee)
 
         // Récupération du canevas et de son contexte
         canevas = document.getElementById('canevas');
+        canevas.width = window.innerWidth;
+        canevas.height = window.innerHeight;
         contexte = canevas.getContext('2d');
         contexte1 = canevas.getContext('2d');
         contexte2 = canevas.getContext('2d');
@@ -83,6 +98,11 @@ const couleur = ["pink", "mistyrose"];
         construireMatrice();
         affichermatrice();
         creationBerry();
+
+        pickup = new Audio ("audio/pickup.wav");
+        fin = new Audio ("audio/fin.wav");
+
+
         
 
     //let ImageSprite1 = new Image(images/CuboneSprites/Cubone_Front)
@@ -103,10 +123,10 @@ const couleur = ["pink", "mistyrose"];
     function construireMatrice(){
         console.log('construire');
 
-        matrice = new Array(58);
+        matrice = new Array(Math.ceil(canevas.width/dimensiontuile));
         for (let i = 0; i < matrice.length; i++)
         {
-            matrice[i] = new Array(29);
+            matrice[i] = new Array(Math.ceil(canevas.height/dimensiontuile));
         }
 
         for(let x = 0; x < matrice.length; x++){
@@ -143,6 +163,7 @@ const couleur = ["pink", "mistyrose"];
         calculerPosition1();
         calculerPosition2();
         rectanglesCollisionJoueur1();
+        rectanglesCollisionJoueur2();
         affichage();  // Appel de la fonction principale d'affichage
         window.requestAnimationFrame(boucleJeu);
         
@@ -155,30 +176,30 @@ const couleur = ["pink", "mistyrose"];
         affichermatrice(); // Dessiner l'arrière-plan des tuiles
         
         if (touche["ArrowUp"]) {
-            contexte.drawImage(joueur1Images.up, posx1, posy1, dimsprite1, dimsprite1);
-        } else if (touche["ArrowDown"]) {
-            contexte.drawImage(joueur1Images.down, posx1, posy1, dimsprite1, dimsprite1);
-        } else if (touche["ArrowLeft"]) {
-            contexte.drawImage(joueur1Images.left, posx1, posy1, dimsprite1, dimsprite1);
-        } else if (touche["ArrowRight"]) {
-            contexte.drawImage(joueur1Images.right, posx1, posy1, dimsprite1, dimsprite1);
-        } else {
-            // Default image when no arrow key is pressed
-            contexte.drawImage(joueur1Images.down, posx1, posy1, dimsprite1, dimsprite1);
-        }
-    
-        // Draw player 2 based on pressed keys
-        if (touche["w"]) {
             contexte.drawImage(joueur2Images.up, posx2, posy2, dimsprite2, dimsprite2);
-        } else if (touche["s"]) {
+        } else if (touche["ArrowDown"]) {
             contexte.drawImage(joueur2Images.down, posx2, posy2, dimsprite2, dimsprite2);
-        } else if (touche["a"]) {
+        } else if (touche["ArrowLeft"]) {
             contexte.drawImage(joueur2Images.left, posx2, posy2, dimsprite2, dimsprite2);
-        } else if (touche["d"]) {
+        } else if (touche["ArrowRight"]) {
             contexte.drawImage(joueur2Images.right, posx2, posy2, dimsprite2, dimsprite2);
         } else {
             // Default image when no arrow key is pressed
             contexte.drawImage(joueur2Images.down, posx2, posy2, dimsprite2, dimsprite2);
+        }
+    
+        // Draw player 2 based on pressed keys
+        if (touche["w"]) {
+            contexte.drawImage(joueur1Images.up, posx1, posy1, dimsprite1, dimsprite1);
+        } else if (touche["s"]) {
+            contexte.drawImage(joueur1Images.down, posx1, posy1, dimsprite1, dimsprite1);
+        } else if (touche["a"]) {
+            contexte.drawImage(joueur1Images.left, posx1, posy1, dimsprite1, dimsprite1);
+        } else if (touche["d"]) {
+            contexte.drawImage(joueur1Images.right, posx1, posy1, dimsprite1, dimsprite1);
+        } else {
+            // Default image when no arrow key is pressed
+            contexte.drawImage(joueur1Images.down, posx1, posy1, dimsprite1, dimsprite1);
         }
 
         // Rendre le joueur 1 (bleu)
@@ -192,7 +213,8 @@ const couleur = ["pink", "mistyrose"];
         for(let i =0; i < tableauBerry.length; i++){         
        
             const Berry = document.getElementById('BerryImg'); 
-            contexte.drawImage(Berry, tableauBerry[i].x, tableauBerry[i].y);         
+            contexte.drawImage(Berry, tableauBerry[i].x, tableauBerry[i].y);  
+                   
         }
     }
     // Fonction appelée lorsqu'une touche est enfoncée
@@ -212,19 +234,22 @@ const couleur = ["pink", "mistyrose"];
 
             console.log("Key pressed:", evenement.key);
         }
-        if ((evenement.key === "Enter")){
-            touche["Enter"] = true;
-            console.log(touche["Enter"]);
-            console.log("Enter!!");
-        }
         if ((evenement.key === " ")){
             touche[" "] = true;
             console.log(touche[" "]);
-            console.log("Spacebar!!");
+            console.log("space!!");
         }
-        if (touche["Enter"] && touche[" "]){
-
+        if ((evenement.key === "1")){
+            touche["1"] = true;
+            console.log(touche["1"]);
+            console.log("1!!");
+        }
+        if (touche[" "] && touche["1"]){
+            
             commencerJeu();
+            console.log("appel commencer jeu")
+            activation = false;
+            
 
         }
     }
@@ -243,80 +268,85 @@ const couleur = ["pink", "mistyrose"];
             console.log('Start!!');
             const debutText = document.getElementById("messageDebutPartie");
             debutText.style.display = "none";
+            if(activation === true){
+             bgm = new Audio ("audio/BGM.mp3");
+            bgm.play();   
+            }
+            
     }
 
 
 
     // Fonction pour calculer la position du joueur 1
     function calculerPosition1(){
-        if (touche["Enter"] && touche[" "]){
+        if (touche[" "] && touche["1"]){
             if(touche["ArrowUp"]){
-                posy1 -= deplacement;
+                posy2 -= deplacement2;
             }
             if(touche["ArrowDown"]){
-                posy1 += deplacement;
+                posy2 += deplacement2;
             }
             if(touche["ArrowRight"]){
-                posx1 += deplacement;
+                posx2 += deplacement2;
             }
             if(touche["ArrowLeft"]){
-                posx1 -= deplacement;
+                posx2 -= deplacement2;
             }
 
-            if(posx1 < 0)
+            if(posx2 < 0)
             {
-             posx1 = 0
+             posx2 = 0
      
             }
             
             // Limites pour éviter que le joueur 1 sorte du canevas
-            if(posx1 > canevas.width - dimsprite1)
+            if(posx2 > canevas.width - dimsprite2)
             {
-             posx1 = canevas.width - dimsprite1
+             posx2 = canevas.width - dimsprite2
             }
      
-            if(posy1 > (canevas.height - dimsprite1) )
+            if(posy2 > (canevas.height - dimsprite2) )
             {
-             posy1 = canevas.height - dimsprite1
+             posy2 = canevas.height - dimsprite2
             }
      
-            if(posy1 < 0 )
+            if(posy2 < 0 )
             {
-             posy1 = 0
+             posy2 = 0
             }
         }
     }
 
     // Fonction pour calculer la position du joueur 2
     function calculerPosition2() {
-        if (touche["Enter"] && touche[" "]){
+        if (touche[" "] && touche["1"]){
             if (touche["w"]) {
-                posy2 -= deplacement2;
+                posy1 -= deplacement;
             }
             if (touche["a"]) {
-                posx2 -= deplacement2;  
+                posx1 -= deplacement;  
             }
             if (touche["s"]) {
-                posy2 += deplacement2;
+                posy1 += deplacement;
             }
             if (touche["d"]) {
-                posx2 += deplacement2;  
+                posx1 += deplacement;  
             }
         
-            if (posx2 < 0) {
-                posx2 = 0;
+            if (posx1 < 0) {
+                posx1 = 0;
             }
         
-            if (posx2 > canevas.width - dimsprite2) {
-                posx2 = canevas.width - dimsprite2;
+            if (posx1 > canevas.width - dimsprite1) {
+                posx1 = canevas.width - dimsprite1;
             }
         
-            if (posy2 > canevas.height - dimsprite2) {
-                posy2 = canevas.height - dimsprite2;
+            if (posy1 > canevas.height - dimsprite1) {
+                posy1 = canevas.height - dimsprite1;
             }
         
-            if (posy2 < 0) {
-                posy2 = 0;
+            if (posy1 < 0) {
+                posy1 = 0;
             }
     
         }       
@@ -328,9 +358,48 @@ const couleur = ["pink", "mistyrose"];
 
         for(let i =0; i < tableauBerry.length; i++ ){
         let A = {x: tableauBerry[i].x, y:tableauBerry[i].y };
-        let B = {x: tableauBerry[i].x + tableauBerry[i].width, y:tableauBerry[i].y + tableauBerry[i].height};
+        let B = {x: tableauBerry[i].x + Berry.width, y:tableauBerry[i].y + Berry.height};
+        
+
         if (rectanglesCollision(A, B, C, D)){
-            console.log("affiche colision")
+            tableauBerry.splice(i, 1);
+            pointageJoueur1++
+            AffichageJoueur1.innerHTML = pointageJoueur1;
+            if(tableauBerry.length <= 1){
+                fin.play();
+                console.log("fin");
+                }
+                else{
+                  pickup.play()  
+                  console.log("pickup");
+                }
+            
+            console.log(pointageJoueur1);
+        }
+       }
+    }
+
+    function rectanglesCollisionJoueur2() {
+        let C = {x: posx2, y:posy2}
+        let D = {x: posx2 + dimsprite2, y:posy2 + dimsprite2}
+
+        for(let i =0; i < tableauBerry.length; i++ ){
+        let A = {x: tableauBerry[i].x, y:tableauBerry[i].y };
+        let B = {x: tableauBerry[i].x + Berry.width, y:tableauBerry[i].y + Berry.height};
+        
+
+        if (rectanglesCollision(A, B, C, D)){
+            tableauBerry.splice(i, 1);
+            pointageJoueur2++
+            AffichageJoueur2.innerHTML = pointageJoueur2;
+            if(tableauBerry.length <= 1){
+                fin.play();
+                console.log("fin");
+                }
+                else{
+                  pickup.play()  
+                  console.log("pickup");
+                }
         }
        }
     }
